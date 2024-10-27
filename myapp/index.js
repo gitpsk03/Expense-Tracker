77e091a72a0c24640f6440f58d503757ca8b217a
gitpsk03 <kalyanshiva526@gmail.com>
2024-10-27 18:11:46 +0530

    message

--- myapp/index.js
+++ myapp/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const path = require('path')
 const {open} = require('sqlite')
+const bcrypt = require('bcrypt')
 const sqlite3 = require('sqlite3')
+const jwt = require('jsonwebtoken')
 
 const app = express()
 app.use(express.json())
@@ -131,4 +133,88 @@ LIMIT
     `
   const summaryArray = await db.all(getSummaryQuery)
   response.send(summaryArray)
+})
+
+
+// Register API
+app.post('/user/', async (request, response) => {
+  const {username, name, password, gender, location} = request.body
+  const hashedPassword = await bcrypt.hash(password, 10)
+  const selectUserQuery = `
+    SELECT * FROM user WHERE username = '${username}'
+  `
+  const dbUser = await db.get(selectUserQuery)
+
+  if (dbUser === undefined) {
+    const createUserQuery = `
+  INSERT INTO
+    user (username, name, password, gender, location)
+  VALUES
+    (
+      '${username}',
+      '${name}',
+      '${hashedPassword}',
+      '${gender}',
+      '${location}'  
+    );`
+    await db.run(createUserQuery)
+    response.send('User Created Successfully')
+  } else {
+    response.status(400)
+    response.send('User Already Exists')
+  }
+})
+
+//login API
+app.post('/login/', async (request, response) => {
+  const {username, password} = request.body
+  const selectUserQuery = `
+    SELECT * FROM user WHERE username = '${username}'
+  `
+  const dbUser = await db.get(selectUserQuery)
+  if (dbUser === undefined) {
+    response.status(400)
+    response.send('Invalid User')
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password)
+    if (isPasswordMatched === true) {
+      const payload = {username: username}
+      const jwtToken = jwt.sign(payload, 'newstring')
+      response.send(jwtToken)
+    } else {
+      response.status(400)
+      response.send('Invalid Password')
+    }
+  }
+})
+
+
+// Get Transactions API with Authentication
+app.get('/transactions/', async (request, response) => {
+  const authHeader = request.headers['authorization']
+  let jwtToken
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(' ')[1]
+  }
+  if (authHeader === undefined) {
+    response.status(401)
+    response.send('Invalid JWT Token')
+  } else {
+    jwt.verify(jwtToken, 'newstring', async (error, payload) => {
+      if (error) {
+        response.status(401)
+        response.send('Invalid JWT Token')
+      } else {
+        const getTransactionsQuery = `
+          SELECT
+            *
+          FROM
+            transactions
+          ORDER BY
+            id;`
+        const transactionsArray = await db.all(getTransactionsQuery)
+        response.send(transactionsArray)
+      }
+    })
+  }
 })
